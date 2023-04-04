d43c53aa70d74522224f995f8a2edb3cb0a4f725
anclark686 <anclark686@gmail.com>
2023-04-04 14:56:12 -0400

    Adding tests

--- school_schedule/student.py
+++ school_schedule/student.py
@@ -1,17 +1,17 @@
 class Student:
     def __init__(self, name, grade, classes):
         self.name = name
-        self.grade = grade
+        self.grade = grade.lower()
         self.classes = classes
     
     def add_class(self, class_name):
         self.classes.append(class_name)
-        return class_name
+        return self
     
     def get_num_classes(self):
         return len(self.classes)
     
     def summary(self):
-        print((f"{self.name} is a {self.grade}. {self.name} " 
+        return(f"{self.name} is a {self.grade}. {self.name} " 
               f"takes {self.get_num_classes()} classes: " 
-              f"{', '.join(self.classes)}"))
+              f"{', '.join(self.classes)}")
--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,4 @@
+import pytest
 from school_schedule.student import Student
 
 def test_instantiate_valid_student():
@@ -15,3 +16,86 @@ def test_instantiate_valid_student():
             )
     assert quinn.name == "Quinn"
     assert len(quinn.classes) == 6
+    assert quinn.grade == "junior"
+
+def test_add_class():
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                [
+                    "Pre-Calc", 
+                    "English III", 
+                    "World History", 
+                    "Gym", 
+                    "Chemistry", 
+                    "Music Composition"
+                ]
+            )
+    
+    quinn.add_class("Biology")
+
+    assert len(quinn.classes) == 7
+
+def test_get_num_classes():
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                [
+                    "Pre-Calc", 
+                    "English III", 
+                    "World History", 
+                    "Gym", 
+                    "Chemistry", 
+                    "Music Composition"
+                ]
+            )
+    class_len = quinn.get_num_classes()
+
+    assert class_len == 6
+
+def test_summary():
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                [
+                    "Pre-Calc", 
+                    "English III", 
+                    "World History", 
+                    "Gym", 
+                    "Chemistry", 
+                    "Music Composition"
+                ]
+            )
+
+    test_str = "Quinn is a junior. Quinn takes 6 classes: Pre-Calc, English III, World History, Gym, Chemistry, Music Composition"
+
+    assert quinn.summary() == test_str
+
+def test_empty_class_list():
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                []
+            )
+    
+    assert len(quinn.classes) == 0
+    assert quinn.get_num_classes() == 0
+
+def test_classes_not_a_list():
+    quinn = Student(
+                "Quinn", 
+                "junior", 
+                "physics"
+            )
+    
+    with pytest.raises(AttributeError):
+        quinn.add_class("chemistry")
+
+def test_grade_not_a_str():
+    
+    with pytest.raises(AttributeError):
+        Student(
+                "Quinn", 
+                10, 
+                []
+            )
