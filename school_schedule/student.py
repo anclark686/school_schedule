0ab69c0437d5858c64bcb06251aa43d786e59c58
anclark686 <anclark686@gmail.com>
2023-04-04 13:57:21 -0400

    initial commit

--- school_schedule/student.py
+++ school_schedule/student.py
@@ -1,3 +1,17 @@
 class Student:
-    pass
+    def __init__(self, name, grade, classes):
+        self.name = name
+        self.grade = grade
+        self.classes = classes
     
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+        return class_name
+    
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        print((f"{self.name} is a {self.grade}. {self.name} " 
+              f"takes {self.get_num_classes()} classes: " 
+              f"{', '.join(self.classes)}"))
